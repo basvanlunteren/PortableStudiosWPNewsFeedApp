b205a82efa50f985020ce137c6d5a981cbe5a79b
Bas van Lunteren <nxg09ryjl0giwieusrcr6g8ofjlpdg@bots.bitbucket.org>
2023-11-06 15:57:46 +1100

    Add Favourites function

--- fe-setup/src/components/default/newsFeed/index.jsx
+++ fe-setup/src/components/default/newsFeed/index.jsx
@@ -17,6 +17,26 @@ const NewsFeed = () => {
     const [filteredResult, setFilteredResult] = useState();
     const [pageSize, setPageSize] = useState(20);
     const [pageNumber, setPageNumber] = useState(1);
+    const [favouritesArray, setfavouritesArray] = useState([]);
+    // const [localFavourites, setLocalFavourites] = useState([]);
+    let tempFavouritesArray = [];
+
+    const makeFavourite = (newsItem, favourite) => {
+        tempFavouritesArray = favouritesArray;
+        if (favourite) {
+            if (tempFavouritesArray.filter((favouriteNewsItem) => favouriteNewsItem.id === newsItem.id).length === 0) {
+                tempFavouritesArray.push(newsItem);
+                setfavouritesArray(tempFavouritesArray);
+            }
+        } else {
+            const markforDeletion = tempFavouritesArray.findIndex((favouriteNewsItem) => favouriteNewsItem.id === newsItem.id);
+            // console.log('markforDeletion', markforDeletion);
+            tempFavouritesArray.splice(markforDeletion, 1);
+            setfavouritesArray(tempFavouritesArray);
+        }
+        console.log('tempFavouritesArray', tempFavouritesArray);
+        localStorage.setItem('storedFavouritesArray', JSON.stringify(tempFavouritesArray));
+    };
 
     const searchFilterFunction = (value) => {
         setSearchTerm(value);
@@ -37,6 +57,8 @@ const NewsFeed = () => {
                 console.log('https://content.guardianapis.com/search?section=world&page=1&page-size=20&api-key=c291fc1f-3319-4ee6-a9c0-69a12842370a:', response.data);
                 console.log('response.data.response.results', response.data);
                 localStorage.setItem('localNewsFeed', JSON.stringify(response.data.response.results));
+                // sectionName
+                // .sort((a, b) => a.time - b.time)
                 setLocalNewsFeed(response.data.response.results);
             })
             .catch(() => {
@@ -45,6 +67,11 @@ const NewsFeed = () => {
     };
 
     useEffect(() => {
+        if (localStorage.getItem('storedFavouritesArray') != null) {
+            setfavouritesArray(JSON.parse(localStorage.getItem('storedFavouritesArray') || '[]'));
+            console.log('localFavourites', JSON.parse(localStorage.getItem('storedFavouritesArray') || '[]'));
+        }
+
         if (localStorage.getItem('localNewsFeed') != null) {
             setLocalNewsFeed(JSON.parse(localStorage.getItem('localNewsFeed') || '[]'));
         } else {
@@ -113,33 +140,87 @@ const NewsFeed = () => {
                             />
                         </label>
                         <br />
-                        {filteredResult && filteredResult.length > 0
+                        {favouritesArray && favouritesArray.length > 0
                             && (
                                 <>
-                                    <ul className="list-group">
-                                        {filteredResult.map((newsItem, index) => (
-                                            <li key={newsItem.id}>
-                                                {index}
-                                                <br />
-                                                <h3>{newsItem.webTitle}</h3>
-                                                <br />
-                                                <a
-                                                    href={newsItem.webUrl}
-                                                    target="_blank"
-                                                    rel="noreferrer"
-                                                >
-                                                    read more
-                                                    <span className="visually-hidden">
-                                                        about&nbsp;
-                                                        {newsItem.webTitle}
-                                                    </span>
-                                                </a>
-                                            </li>
-                                        ))}
+                                    <ul className="favourite-list">
+                                        {favouritesArray.map((localFavNewsItem) => {
+                                            const uniqueID = localFavNewsItem.id.replace(/\\|\//g, '_');
+
+                                            return (
+                                                <li key={uniqueID} style={{ border: '3px solid red' }}>
+                                                    <div className="favourite">
+                                                        <label htmlFor={uniqueID}>
+                                                            <input
+                                                                id={uniqueID}
+                                                                type="checkbox"
+                                                                defaultChecked="true"
+                                                                onChange={(e) => makeFavourite(localFavNewsItem, e.target.checked)}
+                                                            />
+                                                            Favourite
+                                                        </label>
+                                                    </div>
+                                                    <br />
+                                                    <h3>{localFavNewsItem.webTitle}</h3>
+                                                    <br />
+                                                    <a
+                                                        href={localFavNewsItem.webUrl}
+                                                        target="_blank"
+                                                        rel="noreferrer"
+                                                    >
+                                                        read more
+                                                        <span className="visually-hidden">
+                                                            about&nbsp;
+                                                            {localFavNewsItem.webTitle}
+                                                        </span>
+                                                    </a>
+                                                </li>
+                                            );
+                                        })}
                                     </ul>
                                 </>
                             )}
 
+                        {filteredResult && filteredResult.length > 0
+                            && (
+                                <>
+
+                                    <ul className="list-group">
+                                        {filteredResult.map((newsItem) => {
+                                            const uniqueID = newsItem.id;
+
+                                            return (
+                                                <li key={uniqueID}>
+                                                    <div className="favourite">
+                                                        <label htmlFor={uniqueID}>
+                                                            <input
+                                                                id={uniqueID}
+                                                                type="checkbox"
+                                                                onChange={(e) => makeFavourite(newsItem, e.target.checked)}
+                                                            />
+                                                            Favourite
+                                                        </label>
+                                                    </div>
+                                                    <br />
+                                                    <h3>{newsItem.webTitle}</h3>
+                                                    <br />
+                                                    <a
+                                                        href={newsItem.webUrl}
+                                                        target="_blank"
+                                                        rel="noreferrer"
+                                                    >
+                                                        read more
+                                                        <span className="visually-hidden">
+                                                            about&nbsp;
+                                                            {newsItem.webTitle}
+                                                        </span>
+                                                    </a>
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
+                                </>
+                            )}
                     </div>
                 </div>
             </div>
